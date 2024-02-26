0e909d06dac6ad1550ad4927afb7d7af86ab4ac1
Alexandros Tsakiridis <alexandros.tsakiridis2@gmail.com>
2024-02-26 12:40:21 +0200

    cookie loop

---
+++ lab1-1.py
@@ -0,0 +1,17 @@
+import requests
+from http.cookiejar import Cookie
+
+url = "http://facebook.com/"
+
+response = requests.get(url)
+
+# print(response.headers)
+
+# print("SERVER:", response.headers.get("Server"))
+
+for index, cookie in enumerate(response.cookies):
+    print("Cookie", index, ":\t\t", cookie, "\n")
+    for attr in cookie.__dict__:
+        if cookie.__dict__[attr]:
+            print(attr, cookie.__dict__[attr])
+    break

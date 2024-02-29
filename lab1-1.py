29fcac7546086b020ccdebfa8c69a6ae9a832823
Alexandros Tsakiridis <alexandros.tsakiridis2@gmail.com>
2024-02-29 17:32:04 +0200

    addition

--- lab1-1.py
+++ lab1-1.py
@@ -1,19 +1,25 @@
+from datetime import datetime
 import requests
 from http.cookiejar import Cookie
 
-url = "http://facebook.com/"
+url = "https://www.facebook.com"
 
 response = requests.get(url)
+print(response.status_code)
+print(response.headers)
 
-# print(response.headers)
-
-# print("SERVER:", response.headers.get("Server"))
-
+print("SERVER:", response.headers.get("Server"))
+if not response.cookies:
+    print("no cookies received")
 for index, cookie in enumerate(response.cookies):
     print("Cookie", index, ":\t\t", cookie, "\n")
+    print(type(cookie))
     for attr in cookie.__dict__:
         if cookie.__dict__[attr]:
+            if attr == "expires":
+                print(attr, datetime.fromtimestamp(cookie.__dict__[attr] / 1000).strftime("%A, %d %B %Y"))
+                continue
             print(attr, cookie.__dict__[attr])
     break
 
-print("this is a change for the second test branch")
+# print("this is a change for the second test branch")
